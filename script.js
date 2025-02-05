7dd9735f94e0b379ef7330bc2e034d2f103bbd81
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-02-05 16:23:14 +0100

    Move up the important function to the top

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ squares.forEach((square) => {
     })
 });
 
+function createGrid() {
+    let numRow = 16;
+    for (let i = 0; i < numRow; i++) {
+        createRow();
+    }
+}
+
 function createRow() {
     const space = document.querySelector("#canvas");
     const row = document.createElement("div");
@@ -21,12 +28,5 @@ function createRow() {
         square = document.createElement("div");
         square.classList.toggle("square");
         row.appendChild(square);
-    }
-}
-
-function createGrid() {
-    let numRow = 16;
-    for (let i = 0; i < numRow; i++) {
-        createRow();
     }
 }
